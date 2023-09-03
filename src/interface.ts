84443764b051f44b79f51afffc8b07da3d742288
Damiyinliao <2911493102@qq.com>
2023-09-03 23:19:57 +0800

    :sparkles: 新的类型/接口

--- src/interface.ts
+++ src/interface.ts
@@ -4,3 +4,20 @@
 export interface IUserOptions {
   uid: number;
 }
+
+export interface UserContext {
+  userId: number;
+  refreshToken: string;
+}
+
+declare module '@midwayjs/core' {
+  interface Context {
+    userInfo: UserContext;
+    token: string;
+  }
+}
+
+export interface TokenConfig {
+  expire: number;
+  refreshExpire: number;
+}
